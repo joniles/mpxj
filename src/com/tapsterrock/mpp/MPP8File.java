cc919125d7ea80edc76f649df36caba506740948
Jon Iles <joniles@users.sourceforge.net>
2003-05-22 13:15:23 +0000

    Updated to refactor MPP9 code into its own class.

--- src/com/tapsterrock/mpp/MPP8File.java
+++ src/com/tapsterrock/mpp/MPP8File.java
@@ -1,10 +1,26 @@
 /*
  * file:       MPP8File.java
  * author:     Jon Iles
- * copyright:  Tapster Rock Limited
- * date:       08-May-2003
+ * copyright:  (c) Tapster Rock Limited 2002-2003
+ * date:       08/05/2003
  */
  
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+  
 package com.tapsterrock.mpp;
 
 import com.tapsterrock.mpx.AccrueType;
