cfc28ed66a7a0477587485b6b7b26c2bb7eca3b0
Jon Iles <joniles@users.sourceforge.net>
2008-10-24 21:11:24 +0000

    IKVM libraries, and sample .net code.

---
+++ src.net/MpxjConvert.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using net.sf.mpxj;
+using net.sf.mpxj.reader;
+using net.sf.mpxj.writer;
+
+namespace MpxjSample
+{
+    class MpxjConvert
+    {
+        static void Main(string[] args)
+        {
+          try
+          {
+             if (args.Length != 2)
+             {
+                Console.Out.WriteLine ("Usage: MpxjConvert <input file name> <output file name>");
+             }
+             else
+             {
+                MpxjConvert convert = new MpxjConvert();
+                convert.process(args[0], args[1]);
+             }
+          }
+
+          catch (Exception ex)
+          {
+              Console.Out.WriteLine(ex.ToString());
+          }
+        }
+
+        public void process (string inputFile, string outputFile)
+        {
+            Console.Out.WriteLine("Reading input file started.");
+            DateTime start = DateTime.Now;
+            ProjectReader reader = ProjectReaderUtility.getProjectReader(inputFile);
+            ProjectFile projectFile = reader.read(inputFile);
+            long elapsed = DateTime.Now.Ticks - start.Ticks;
+            Console.Out.WriteLine("Reading input file completed in " + elapsed + "ms.");
+
+            Console.Out.WriteLine("Writing output file started.");
+            start = DateTime.Now;
+            ProjectWriter writer = ProjectWriterUtility.getProjectWriter(outputFile);
+            writer.write(projectFile, outputFile);
+            elapsed = DateTime.Now.Ticks - start.Ticks;
+            Console.Out.WriteLine("Writing output completed in " + elapsed + "ms.");
+        }
+    }
+}
---
+++ src.net/MpxjTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using junit.framework;
+using junit.textui;
+using net.sf.mpxj.junit;
+
+namespace MpxjSample
+{
+    class MpxjTest
+    {
+        static void Main(string[] args)
+        {
+            java.lang.System.setProperty("mpxj.junit.datadir", "c:\\java\\mpxj\\junit\\data");
+            Test suite = new MPXJTest();
+            TestRunner.runAndWait(suite);
+        }
+    }
+}
