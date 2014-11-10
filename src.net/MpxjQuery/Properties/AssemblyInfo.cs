9fd0dbdc7c6ff7dbf9e58e96cc60a5073c96f81d
Jon Iles <joniles@users.sourceforge.net>
2014-11-10 12:22:31 +0000

    Updated to automatically generate and package MpxjUtilities.dll

--- src.net/MpxjQuery/Properties/AssemblyInfo.cs
+++ src.net/MpxjQuery/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MpxjQuery")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Davis Langdon")]
-[assembly: AssemblyProduct("MpxjQuery")]
-[assembly: AssemblyCopyright("Copyright © Davis Langdon 2012")]
+[assembly: AssemblyCompany("Packwood Software")]
+[assembly: AssemblyProduct("MPXJ")]
+[assembly: AssemblyCopyright("Copyright © Packwood Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("4.6.1.0")]
+[assembly: AssemblyVersion("4.6.1.0")]
+[assembly: AssemblyFileVersion("4.6.1.0")]
