38244f44d0e78acc57df594bae69fb0107a46415
Jon Iles <jon.iles@bcs.org.uk>
2014-11-18 22:41:17 +0000

    Converted FileVersion class to enum

--- src/net/sf/mpxj/FileVersion.java
+++ src/net/sf/mpxj/FileVersion.java
@@ -26,8 +26,12 @@ package net.sf.mpxj;
 /**
  * Instances of this class represent enumerated file version values.
  */
-public final class FileVersion
+public enum FileVersion implements MpxjEnum
 {
+   VERSION_1_0(1),
+   VERSION_3_0(3),
+   VERSION_4_0(4);
+
    /**
     * Private constructor.
     *
@@ -43,7 +47,7 @@ public final class FileVersion
     *
     * @return file version value
     */
-   public int getValue()
+   @Override public int getValue()
    {
       return (m_value);
    }
@@ -114,19 +118,4 @@ public final class FileVersion
    }
 
    private int m_value;
-
-   /**
-    * Constant representing file version.
-    */
-   public static final FileVersion VERSION_1_0 = new FileVersion(1);
-
-   /**
-    * Constant representing file version.
-    */
-   public static final FileVersion VERSION_3_0 = new FileVersion(3);
-
-   /**
-    * Constant representing file version.
-    */
-   public static final FileVersion VERSION_4_0 = new FileVersion(4);
 }
