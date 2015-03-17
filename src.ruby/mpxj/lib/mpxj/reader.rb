757f4157ccfdaf822d31cdb8a7cda4ecb8becbda
Jon Iles <jon.iles@bcs.org.uk>
2015-03-17 23:29:12 +0000

    Correct path list separator. All filenames quoted.

--- src.ruby/mpxj/lib/mpxj/reader.rb
+++ src.ruby/mpxj/lib/mpxj/reader.rb
@@ -6,8 +6,8 @@ module MPXJ
       project = nil
       json_file = Tempfile.new([File.basename(file_name, ".*"), '.json'])
       begin
-        classpath = Dir["#{File.dirname(__FILE__)}/*.jar"].join(";")
-        java_output = `java -cp \"#{classpath}\" net.sf.mpxj.sample.MpxjConvert #{file_name} #{json_file.path}`
+        classpath = Dir["#{File.dirname(__FILE__)}/*.jar"].join(path_separator)
+        java_output = `java -cp \"#{classpath}\" net.sf.mpxj.sample.MpxjConvert \"#{file_name}\" \"#{json_file.path}\"`
         if $?.exitstatus != 0
           raise "Failed to read file: #{java_output}"
         end
@@ -17,6 +17,18 @@ module MPXJ
         json_file.unlink
       end
       project
+    end
+
+    def self.path_separator
+      if windows?
+        ";"
+      else
+        ":"
+      end
+    end
+
+    def self.windows?
+      (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
     end
   end
 end
