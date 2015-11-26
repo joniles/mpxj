ad3a4a73bc530374cf9078e7ea8c9fac4f1ed589
Jon Iles <jon.iles@bcs.org.uk>
2015-11-26 10:33:33 +0000

    Updated version.

--- src.net/MpxjUtilities/Properties/AssemblyInfo.cs
+++ src.net/MpxjUtilities/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("5.1.12.0")]
-[assembly: AssemblyVersion("5.1.12.0")]
-[assembly: AssemblyFileVersion("5.1.12.0")]
+// [assembly: AssemblyVersion("5.1.13.0")]
+[assembly: AssemblyVersion("5.1.13.0")]
+[assembly: AssemblyFileVersion("5.1.13.0")]
