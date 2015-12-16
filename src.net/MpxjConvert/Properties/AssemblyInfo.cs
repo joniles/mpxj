1f3a0674a031a51fd1965d8f8688610fe6b75d1b
Jon Iles <jon.iles@bcs.org.uk>
2015-12-16 16:58:43 +0000

    Updated version number.

--- src.net/MpxjConvert/Properties/AssemblyInfo.cs
+++ src.net/MpxjConvert/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("5.1.14.0")]
-[assembly: AssemblyVersion("5.1.14.0")]
-[assembly: AssemblyFileVersion("5.1.14.0")]
+// [assembly: AssemblyVersion("5.1.15.0")]
+[assembly: AssemblyVersion("5.1.15.0")]
+[assembly: AssemblyFileVersion("5.1.15.0")]
--- src.ruby/mpxj/lib/mpxj/version.rb
+++ src.ruby/mpxj/lib/mpxj/version.rb
@@ -1,5 +1,5 @@
 # MPXJ gem module
 module MPXJ
   # MPXJ gem version number
-  VERSION = "5.1.14"
+  VERSION = "5.1.15"
 end
