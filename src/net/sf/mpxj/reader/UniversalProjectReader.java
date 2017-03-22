3f085eb67448de640247df685ccd330d32c0e821
Jon Iles <jon.iles@bcs.org.uk>
2017-03-22 10:03:06 +0000

    Improve support for Phoenix Project Manager XML files

--- src/net/sf/mpxj/reader/UniversalProjectReader.java
+++ src/net/sf/mpxj/reader/UniversalProjectReader.java
@@ -613,5 +613,5 @@ public class UniversalProjectReader extends AbstractProjectReader
 
    private static final Pattern MSPDI_FINGERPRINT = Pattern.compile(".*xmlns=\"http://schemas\\.microsoft\\.com/project\".*", Pattern.DOTALL);
 
-   private static final Pattern PHOENIX_XML_FINGERPRINT = Pattern.compile(".*application=\"Phoenix Project Manager\".*", Pattern.DOTALL);
+   private static final Pattern PHOENIX_XML_FINGERPRINT = Pattern.compile(".*<project.*version=\"(\\d+|\\d+\\.\\d+)\".*update_mode=\"(true|false)\".*>.*", Pattern.DOTALL);
 }
