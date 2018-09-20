d17b37ebe2569ba2e2236496bf4ab75fa95b0142
Jon Iles <jon.iles@bcs.org.uk>
2018-09-20 01:25:14 +0100

    Work in progress

--- src/net/sf/mpxj/synchro/TaskReader.java
+++ src/net/sf/mpxj/synchro/TaskReader.java
@@ -34,11 +34,6 @@ class TaskReader extends TableReader
       relationReader.read();
       map.put("RELATIONS", relationReader.getRows());
 
-      //      byte[] block1b = new byte[16];
-      //      m_stream.read(block1b);
-      //      System.out.println("BLOCK1B");
-      //      System.out.println(MPPUtility.hexdump(block1b, true, 16, ""));
-
       map.put("CALENDAR_UUID", SynchroUtility.getUUID(m_stream));
       System.out.println("Calendar UUID: " + map.get("CALENDAR_UUID"));
 
@@ -75,11 +70,6 @@ class TaskReader extends TableReader
          map.put("COSTS", costReader.getRows());
       }
 
-      //      byte[] block2a = new byte[35];
-      //      m_stream.read(block2a);
-      //      System.out.println("BLOCK2A");
-      //      System.out.println(MPPUtility.hexdump(block2a, true, 16, ""));
-
       readDatePair(map, "UNKNOWN_DATE1");
       readDatePair(map, "UNKNOWN_DATE2");
       readDatePair(map, "UNKNOWN_DATE3");
@@ -95,11 +85,6 @@ class TaskReader extends TableReader
          commentaryReader.read();
          map.put("COMMENTARY", commentaryReader.getRows());
       }
-
-      //      byte[] block3 = new byte[140];
-      //      m_stream.read(block3);
-      //      System.out.println("BLOCK3");
-      //      System.out.println(MPPUtility.hexdump(block3, true, 16, ""));
 
       byte[] block2b = new byte[4];
       m_stream.read(block2b);
@@ -121,11 +106,6 @@ class TaskReader extends TableReader
          }
       }
 
-      //      byte[] block3 = new byte[106];
-      //      m_stream.read(block3);
-      //      System.out.println("BLOCK3");
-      //      System.out.println(MPPUtility.hexdump(block3, true, 16, ""));
-
       byte[] block31 = new byte[8];
       m_stream.read(block31);
       System.out.println("BLOCK31");
@@ -145,54 +125,48 @@ class TaskReader extends TableReader
       map.put("URL", SynchroUtility.getString(m_stream));
       System.out.println("URL: " + map.get("URL"));
 
-      // was 24
-      byte[] block3a = new byte[4];
-      m_stream.read(block3a);
-      System.out.println("BLOCK3A");
-      System.out.println(MPPUtility.hexdump(block3a, true, 16, ""));
+      //      // Not sure if  this is 2 bytes or 1
+      //      String progressType;
+      //      switch (block3a[0])
+      //      {
+      //         case 1:
+      //         {
+      //            progressType = "Automatic";
+      //            break;
+      //         }
+      //
+      //         case 2:
+      //         {
+      //            progressType = "Manual";
+      //            break;
+      //         }
+      //
+      //         case 3:
+      //         {
+      //            progressType = "Duration";
+      //            break;
+      //         }
+      //
+      //         case 4:
+      //         {
+      //            progressType = "Physical";
+      //            break;
+      //         }
+      //
+      //         case 5:
+      //         {
+      //            progressType = "Unit";
+      //            break;
+      //         }
+      //
+      //         default:
+      //         {
+      //            progressType = null;
+      //            break;
+      //         }
+      //      }
 
-      // Not sure if  this is 2 bytes or 1
-      String progressType;
-      switch (block3a[0])
-      {
-         case 1:
-         {
-            progressType = "Automatic";
-            break;
-         }
-
-         case 2:
-         {
-            progressType = "Manual";
-            break;
-         }
-
-         case 3:
-         {
-            progressType = "Duration";
-            break;
-         }
-
-         case 4:
-         {
-            progressType = "Physical";
-            break;
-         }
-
-         case 5:
-         {
-            progressType = "Unit";
-            break;
-         }
-
-         default:
-         {
-            progressType = null;
-            break;
-         }
-      }
-
-      map.put("PROGRESS_TYPE", progressType);
+      map.put("PROGRESS_TYPE", SynchroUtility.getInteger(m_stream));
       System.out.println("Progress Type: " + map.get("PROGRESS_TYPE"));
 
       map.put("PERCENT_COMPLETE", SynchroUtility.getDouble(m_stream));
@@ -213,14 +187,6 @@ class TaskReader extends TableReader
          map.put("USER_FIELDS", reader.getRows());
       }
 
-      //      map.put("REMAINING_DURATION", SynchroUtility.getDuration(m_stream));
-      //      System.out.println("Task Remaining Duration: " + map.get("REMAINING_DURATION"));
-
-      //      byte[] block4 = new byte[120];
-      //      m_stream.read(block4);
-      //      System.out.println("BLOCK4");
-      //      System.out.println(MPPUtility.hexdump(block4, true, 16, ""));
-
       byte[] block41 = new byte[28];
       m_stream.read(block41);
       System.out.println("BLOCK41");
@@ -239,18 +205,14 @@ class TaskReader extends TableReader
       map.put("PHYSICAL_QUANTITY", SynchroUtility.getDouble(m_stream));
       System.out.println("Task Physical Quantity: " + map.get("PHYSICAL_QUANTITY"));
 
-      byte[] block43 = new byte[8];
-      m_stream.read(block43);
-      System.out.println("BLOCK43");
-      System.out.println(MPPUtility.hexdump(block43, true, 16, ""));
+      map.put("REMAINING_PHYSICAL_QUANTITY", SynchroUtility.getDouble(m_stream));
+      System.out.println("Task Remaining Physical Quantity: " + map.get("REMAINING_PHYSICAL_QUANTITY"));
 
       map.put("PHYSICAL_QUANTITY_UNIT", SynchroUtility.getInteger(m_stream));
       System.out.println("Task Physical Quantity Unit: " + map.get("PHYSICAL_QUANTITY_UNIT"));
 
-      byte[] block44 = new byte[8];
-      m_stream.read(block44);
-      System.out.println("BLOCK44");
-      System.out.println(MPPUtility.hexdump(block44, true, 16, ""));
+      map.put("ACTUAL_PHYSICAL_QUANTITY", SynchroUtility.getDouble(m_stream));
+      System.out.println("Task Actual Physical Quantity: " + map.get("ACTUAL_PHYSICAL_QUANTITY"));
 
       readDatePair(map, "EXPECTED_FINISH");
 
