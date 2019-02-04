a48991e050fb43ed834c48b58235ef313d426fdb
Jon Iles <jon.iles@bcs.org.uk>
2019-02-04 18:35:22 +0000

    Special case for 100% complete milestones

--- src/main/java/net/sf/mpxj/mspdi/MSPDIWriter.java
+++ src/main/java/net/sf/mpxj/mspdi/MSPDIWriter.java
@@ -1482,7 +1482,15 @@ public final class MSPDIWriter extends AbstractProjectWriter
             dummy.setWork(duration);
             dummy.setActualWork(Duration.getInstance(actualWork, durationUnits));
             dummy.setRemainingWork(Duration.getInstance(remainingWork, durationUnits));
-
+            dummy.setPercentageWorkComplete(task.getPercentageComplete());
+            dummy.setActualStart(task.getActualStart());
+            
+            // Without this, MS Project will mark a 100% complete milestone as 99% complete
+            if (percentComplete == 100 && duration.getDuration() == 0)
+            {              
+               dummy.setActualFinish(task.getActualStart());
+            }
+            
             list.add(writeAssignment(dummy));
          }
       }
