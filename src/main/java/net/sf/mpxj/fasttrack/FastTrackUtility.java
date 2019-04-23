6dd9ff51b992745bbc606f077f687d72f91704b4
Jon Iles <jon.iles@bcs.org.uk>
2019-04-23 11:50:57 +0100

    Fix typo

--- src/main/java/net/sf/mpxj/fasttrack/FastTrackUtility.java
+++ src/main/java/net/sf/mpxj/fasttrack/FastTrackUtility.java
@@ -49,7 +49,7 @@ final class FastTrackUtility
     * Ensure that an array index is in range.
     *
     * @param buffer array of data
-    * @param offset index into arry
+    * @param offset index into array
     */
    public static final void validateOffset(byte[] buffer, int offset)
    {
