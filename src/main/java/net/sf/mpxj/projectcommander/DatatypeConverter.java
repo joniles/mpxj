6004e6bec4837018901559fe2ae4f0437e4e288d
Jon Iles <jon.iles@bcs.org.uk>
2020-05-15 08:44:09 +0100

    WIP

--- src/main/java/net/sf/mpxj/projectcommander/DatatypeConverter.java
+++ src/main/java/net/sf/mpxj/projectcommander/DatatypeConverter.java
@@ -1,4 +1,9 @@
+
 package net.sf.mpxj.projectcommander;
+
+import net.sf.mpxj.Duration;
+import net.sf.mpxj.RelationType;
+import net.sf.mpxj.TimeUnit;
 
 class DatatypeConverter
 {
@@ -19,7 +24,34 @@ class DatatypeConverter
       }
       return result;
    }
-   
+
+   public static final int getInt(byte[] data, int offset)
+   {
+      int result = 0;
+      int i = offset;
+      for (int shiftBy = 0; shiftBy < 32; shiftBy += 8)
+      {
+         result |= ((data[i] & 0xff)) << shiftBy;
+         ++i;
+      }
+      return result;
+   }
+
+   public static final int getShort(byte[] data, int offset, int defaultValue)
+   {
+      return offset + 2 > data.length ? defaultValue : getShort(data, offset);
+   }
+
+   public static final int getInt(byte[] data, int offset, int defaultValue)
+   {
+      return offset + 4 > data.length ? defaultValue : getInt(data, offset);
+   }
+
+   public static final String getString(byte[] data, int offset, String defaultValue)
+   {
+      return offset >= data.length ? defaultValue : getString(data, offset);
+   }
+
    public static final String getTwoByteLengthString(byte[] data, int offset)
    {
       int length = getShort(data, offset);
@@ -30,23 +62,64 @@ class DatatypeConverter
       }
       else
       {
-         result = new String(data, offset+2, length);
+         result = new String(data, offset + 2, length);
       }
       return result;
    }
-   
+
    public static final String getString(byte[] data, int offset)
    {
       int length = getByte(data, offset);
       String result;
-      if (length == 0)
+      if (length == 0 || length+offset+1 > data.length)
       {
          result = null;
       }
       else
       {
-         result = new String(data, offset+1, length);
+         result = new String(data, offset + 1, length);
       }
+      return result;
+   }
+
+   public static final Duration getDuration(byte[] data, int offset)
+   {
+      int durationInMinutes = getInt(data, offset, 0);
+      return Duration.getInstance(durationInMinutes / 60, TimeUnit.HOURS);
+   }
+
+   public static final RelationType getRelationType(byte[] data, int offset)
+   {
+      RelationType result;
+      int value = getByte(data, offset);
+      switch (value)
+      {
+         case 5:
+         {
+            result = RelationType.START_FINISH;
+            break;
+         }
+         
+         case 6:
+         {
+            result = RelationType.START_START;
+            break;
+         }
+
+         case 7:
+         {
+            result = RelationType.FINISH_FINISH;
+            break;
+         }
+
+         case 8:
+         default:
+         {
+            result = RelationType.FINISH_START;
+            break;
+         }
+      }
+
       return result;
    }
 }
--- src/main/java/net/sf/mpxj/projectcommander/ProjectCommanderReader.java
+++ src/main/java/net/sf/mpxj/projectcommander/ProjectCommanderReader.java
@@ -5,14 +5,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import net.sf.mpxj.Duration;
 import net.sf.mpxj.EventManager;
 import net.sf.mpxj.MPXJException;
 import net.sf.mpxj.ProjectCalendar;
 import net.sf.mpxj.ProjectFile;
+import net.sf.mpxj.RelationType;
 import net.sf.mpxj.Resource;
 import net.sf.mpxj.Task;
 import net.sf.mpxj.common.ByteArrayHelper;
+import net.sf.mpxj.common.NumberHelper;
 import net.sf.mpxj.listener.ProjectListener;
 import net.sf.mpxj.reader.AbstractProjectReader;
 
@@ -70,10 +75,11 @@ public final class ProjectCommanderReader extends AbstractProjectReader
       m_data.getBlocks().stream().filter(block -> "CCalendar".equals(block.getName())).forEach(block -> readCalendar(block));
    }
 
-
    private void readTasks()
    {
-      m_data.getBlocks().stream().filter(block -> "CTask".equals(block.getName())).forEach(block -> readTask(block));
+      Map<Integer, Integer> childTaskCounts = new TreeMap<>();
+      m_data.getBlocks().stream().filter(block -> "CTask".equals(block.getName())).forEach(block -> readTask(block, childTaskCounts));
+      createHierarchy(childTaskCounts);
    }
 
    private void readResources()
@@ -84,24 +90,133 @@ public final class ProjectCommanderReader extends AbstractProjectReader
    private void readCalendar(Block block)
    {
       byte[] data = block.getData();
-      ProjectCalendar calendar = m_projectFile.addCalendar();
-      calendar.setName(DatatypeConverter.getString(data, 0));
-      m_eventManager.fireCalendarReadEvent(calendar);
+      String name = DatatypeConverter.getString(data, 0, null);
+      if (name != null)
+      {
+         ProjectCalendar calendar = m_projectFile.addCalendar();
+         calendar.setName(name);
+         m_eventManager.fireCalendarReadEvent(calendar);
+      }
    }
 
-   private void readTask(Block block)
+   private void readTask(Block block, Map<Integer, Integer> childTaskCounts)
    {
-      byte[] data = block.getData();
-      Task task = m_projectFile.addTask();
-      task.setName(DatatypeConverter.getString(data, 0));
-      m_eventManager.fireTaskReadEvent(task);
+      int offset = 0;
+      byte[] cTaskData = block.getData();
+      String name = DatatypeConverter.getString(cTaskData, offset, null);
+      if (name == null)
+      {
+         return;
+      }
+     
+      Block cUsageTask = getChildBlock(block, "CUsageTask");
+
       
-      int offset = (task.getName() == null ? 0 : task.getName().length()) + 1;
-      int length = data.length - offset;
-      System.out.println(task.getID() + "\t" + ByteArrayHelper.hexdump(data, offset, length, true));
-      /*
-       * Summary tasks are shorter. The last two bytes is a count of the number of child tasks, the next n gasks are the child tasks
-       */
+      byte[] cBaselineData = getByteArray(block, "CBaselineData");
+      byte[] cBarData = getByteArray(block, "CBar");
+      byte[] cUsageTaskData = getByteArray(block, "CUsageTask");
+      byte[] cUsageTaskBaselineData = getByteArray(cUsageTask, "CBaselineData");
+
+      Task task = m_projectFile.addTask();
+      task.setName(name);
+      m_eventManager.fireTaskReadEvent(task);
+
+      offset += ((task.getName() == null ? 0 : task.getName().length()) + 1);
+
+      // Skip the task name when dumping data
+      //System.out.println(task.getID() + "\t" + ByteArrayHelper.hexdump(cTaskData, offset, cTaskData.length - offset, false));
+      //System.out.println(task.getID() + "\t" + ByteArrayHelper.hexdump(cBarData, 0, cBarData.length, false) + " length=" + cBarData.length);
+      //System.out.println(task.getID() + "\t" + ByteArrayHelper.hexdump(cBaselineData, 0, cBaselineData.length, false) + " length=" + cBaselineData.length);
+      //System.out.println(task.getID() + "\t" + ByteArrayHelper.hexdump(cUsageTaskData, 0, cUsageTaskData.length, false) + " length=" + cUsageTaskData.length);            
+      //System.out.println(task.getID() + "\t" + ByteArrayHelper.hexdump(cUsageTaskBaselineData, 0, cUsageTaskBaselineData.length, false) + " length=" + cUsageTaskBaselineData.length);
+
+      int childTaskCount = DatatypeConverter.getShort(cTaskData, offset + 405, 0);
+      if (childTaskCount != 0)
+      {
+         childTaskCounts.put(task.getID(), Integer.valueOf(DatatypeConverter.getShort(cTaskData, offset + 405, 0)));
+      }
+
+      // NOTE: summary tasks don't appear to have their own Unique ID values
+      // Sumary task don't have CBar, CBaselineData, CUsageTask
+      // The task Unique ID appears to be present in CBar and CBaselineData
+      // Duration at CUsageTask 366????
+
+      // Start Date      
+      //      long timestampInSeconds = DatatypeConverter.getInt(cBarData, 5, 0);
+      //      long timestampInMilliseconds = timestampInSeconds * 1000; 
+      //      System.out.println(task.getID() + "\t" + DateHelper.getTimestampFromLong(timestampInMilliseconds));
+
+      // This is the task unique ID as used by CLink
+      //System.out.println(task.getID() + "\t" + DatatypeConverter.getShort(cBarData, 23, 0));
+      
+// Other apparently unique identifiers      
+      //      System.out.println(task.getID() + "\t" + DatatypeConverter.getShort(cBaselineData, 69, 0));
+      //System.out.println(task.getID() + "\t" + DatatypeConverter.getShort(cUsageTaskData, 408, 0));
+      //System.out.println(task.getID() + "\t" + DatatypeConverter.getShort(cUsageTaskBaselineData, 69, 0));
+
+      // Duration
+      // System.out.println(task.getID() + "\t" + DatatypeConverter.getDuration(cBaselineData, 366));
+
+      block.getChildBlocks().stream().filter(x -> "CLink".equals(x.getName())).forEach(x -> dumpLink(task, x));
+   }
+
+   private byte[] getByteArray(Block block, String name)
+   {
+      Block childBlock = getChildBlock(block, name);
+      return childBlock == null ? EMPTY_BYTE_ARRAY : childBlock.getData();
+   }
+
+   private Block getChildBlock(Block block, String name)
+   {
+      Block result;
+      if (block == null)
+      {
+         result = null;
+      }
+      else
+      {
+         result = block.getChildBlocks().stream().filter(x -> name.equals(x.getName())).findFirst().orElse(null);
+      }
+      return result;
+   }
+
+   private void dumpLink(Task task, Block block)
+   {
+      // 14 bytes or less, predecessor
+      // more than 14 bytes successor
+
+      byte[] data = block.getData();
+      if (data.length > 14)
+      {
+         int taskID = DatatypeConverter.getShort(data, 0);
+         Duration lag = DatatypeConverter.getDuration(data, 6);
+         RelationType type = DatatypeConverter.getRelationType(data, 2);
+         System.out.println(task.getID() + "\t" + taskID + "\t" + type + "\t" + lag + "\t" + ByteArrayHelper.hexdump(data, 0, data.length, false));
+      }
+   }
+
+   private void createHierarchy(Map<Integer, Integer> childTaskCounts)
+   {
+      for (Map.Entry<Integer, Integer> entry : childTaskCounts.entrySet())
+      {
+         Task task = m_projectFile.getTaskByID(entry.getKey());
+         int startID = task.getID().intValue() + 1;
+         int endID = startID + entry.getValue().intValue();
+
+         for (int id = startID; id < endID; id++)
+         {
+            Task childTask = m_projectFile.getTaskByID(Integer.valueOf(id));
+            if (childTask != null)
+            {
+               childTask.setOutlineLevel(Integer.valueOf(NumberHelper.getInt(childTask.getOutlineLevel()) + 1));
+            }
+            else
+            {
+               System.out.println("skip " +id);
+            }
+         }
+      }
+      m_projectFile.getTasks().updateStructure();
    }
 
    private void readResource(Block block)
@@ -109,11 +224,13 @@ public final class ProjectCommanderReader extends AbstractProjectReader
       byte[] data = block.getData();
       Resource resource = m_projectFile.addResource();
       resource.setName(DatatypeConverter.getString(data, 0));
-      m_eventManager.fireResourceReadEvent(resource);            
+      m_eventManager.fireResourceReadEvent(resource);
    }
 
    private ProjectFile m_projectFile;
    private EventManager m_eventManager;
    private List<ProjectListener> m_projectListeners;
    private ProjectCommanderData m_data;
+
+   private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
 }
