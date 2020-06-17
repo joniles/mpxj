60e67202a906afd230bffd92fec092baaa8b8d14
Jon Iles <jon.iles@bcs.org.uk>
2020-06-17 11:28:29 +0100

    WIP

--- src/main/java/net/sf/mpxj/synchro/UserFieldReader.java
+++ src/main/java/net/sf/mpxj/synchro/UserFieldReader.java
@@ -43,16 +43,20 @@ class UserFieldReader extends TableReader
 
    @Override protected void readRow(StreamReader stream, Map<String, Object> map) throws IOException
    {
-      if (stream.getVersion().atLeast(Synchro.VERSION_6_0_0))
+      if (stream.getVersion().atLeast(Synchro.VERSION_6_2_0))
       {
-         map.put("VALUE", stream.readString());
-         map.put("UNKNOWN1", stream.readBytes(26));
+         readVersion62Row(stream, map);
       }
       else
       {
-         map.put("UNKNOWN1", stream.readBytes(16));
-         map.put("VALUE", stream.readString());
-         map.put("UNKNOWN2", stream.readBytes(26));
+         if (stream.getVersion().atLeast(Synchro.VERSION_6_0_0))
+         {
+            readVersion60Row(stream, map);
+         }
+         else
+         {
+            readVersion50Row(stream, map);
+         }
       }
    }
 
@@ -64,5 +68,93 @@ class UserFieldReader extends TableReader
    @Override protected int rowMagicNumber()
    {
       return 0x440A7BA3;
+   }
+
+   private void readVersion62Row(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      int type = stream.readInt();
+
+      switch (type)
+      {
+         case 2:
+         {
+            readVersion62BooleanRow(stream, map);
+            break;
+         }
+
+         case 3:
+         {
+            readVersion62IntegerRow(stream, map);
+            break;
+         }
+
+         case 4:
+         {
+            readVersion62NumberRow(stream, map);
+            break;
+         }
+
+         case 5:
+         {
+            readVersion62DateRow(stream, map);
+            break;
+         }
+
+         case 6:
+         {
+            readVersion62StringRow(stream, map);
+            break;
+         }
+
+         default:
+         {
+            throw new IllegalArgumentException("Unexpected file format");
+         }
+      }
+   }
+
+   private void readVersion62BooleanRow(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      map.put("VALUE", stream.readBoolean());
+      map.put("UNKNOWN0", stream.readBytes(1));
+      map.put("UNKNOWN1", stream.readBytes(26));
+   }
+
+   private void readVersion62IntegerRow(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      map.put("VALUE", stream.readInteger());
+      map.put("UNKNOWN1", stream.readBytes(26));
+   }
+
+   private void readVersion62NumberRow(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      map.put("VALUE", stream.readDouble());
+      map.put("UNKNOWN1", stream.readBytes(26));
+   }
+
+   private void readVersion62StringRow(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      map.put("VALUE", stream.readString());
+      map.put("UNKNOWN1", stream.readBytes(26));
+   }
+
+   private void readVersion62DateRow(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      map.put("VALUE", stream.readDate());
+      map.put("UNKNOWN0", stream.readInteger());
+      map.put("UNKNOWN1", stream.readBytes(26));
+   }
+
+   private void readVersion60Row(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      map.put("VALUE", stream.readString());
+      map.put("UNKNOWN1", stream.readBytes(26));
+   }
+
+   private void readVersion50Row(StreamReader stream, Map<String, Object> map) throws IOException
+   {
+      map.put("UNKNOWN1", stream.readBytes(16));
+      map.put("VALUE", stream.readString());
+      map.put("UNKNOWN2", stream.readBytes(26));
    }
 }
