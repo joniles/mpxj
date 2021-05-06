0221d974f830e87361a9a31cb7952a7f21de8864
Jon Iles <jon.iles@bcs.org.uk>
2021-05-06 13:55:20 +0100

    WIP

--- src/main/java/net/sf/mpxj/primavera/PrimaveraPMProjectWriter.java
+++ src/main/java/net/sf/mpxj/primavera/PrimaveraPMProjectWriter.java
@@ -346,9 +346,11 @@ final class PrimaveraPMProjectWriter
       if (!workContours.isEmpty())
       {
          ObjectSequence id = new ObjectSequence(1);
-
+         List<WorkContour> sortedWorkContours = new ArrayList<>(workContours);
+         Collections.sort(sortedWorkContours, (c1, c2) -> c1.getName().compareTo(c2.getName()));
+         
          List<ResourceCurveType> curves = m_apibo.getResourceCurve();
-         for (WorkContour contour : workContours)
+         for (WorkContour contour : sortedWorkContours)
          {
             ResourceCurveType curve = m_factory.createResourceCurveType();
             curves.add(curve);
