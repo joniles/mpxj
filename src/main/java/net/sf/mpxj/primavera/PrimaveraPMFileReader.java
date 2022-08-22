6c9621cc069609ed9c1c2e6145e1bc9f187678b5
Jon Iles <jon.iles@bcs.org.uk>
2022-08-22 19:09:39 +0100

    WIP

--- src/main/java/net/sf/mpxj/primavera/PrimaveraPMFileReader.java
+++ src/main/java/net/sf/mpxj/primavera/PrimaveraPMFileReader.java
@@ -1905,7 +1905,7 @@ public final class PrimaveraPMFileReader extends AbstractProjectStreamReader
     * @param apibo xml container
     */
    private void processRoleRates(APIBusinessObjects apibo)
-   {      
+   {
       List<RoleRateType> rates = new ArrayList<>(apibo.getRoleRateNew().isEmpty() ? apibo.getRoleRate() : apibo.getRoleRateNew());
 
       // Primavera defines resource cost tables by start dates so sort and define end by next
