03022f8b6f63627aba91de7646a5a67d8243aee8
Jon Iles <jon.iles@bcs.org.uk>
2022-11-18 16:31:49 +0000

    Handle unknown FTS assignment structure (#392)

--- src/main/java/net/sf/mpxj/fasttrack/AssignmentColumn.java
+++ src/main/java/net/sf/mpxj/fasttrack/AssignmentColumn.java
@@ -48,7 +48,16 @@ class AssignmentColumn extends AbstractColumn
          m_options = options.getData();
          offset = options.getOffset();
 
-         // Skip bytes
+         // Handle unknown string structure seen in a couple of examples from v11/2020
+         if (FastTrackUtility.getByte(buffer, offset+1) == 0x01)
+         {
+            offset += 4;
+            int stringLength = FastTrackUtility.getInt(buffer, offset);
+            offset += 4;
+            // FastTrackUtility.getString(buffer, offset, stringLength);
+            offset += stringLength;
+         }
+
          offset += 8;
       }
 
