a77c0140696c9405876e2dc61c89e31a6aecff3b
joniles <jon.iles@bcs.org.uk>
2023-02-04 18:16:09 +0000

    WIP

--- src/main/java/net/sf/mpxj/asta/ObjectType.java
+++ src/main/java/net/sf/mpxj/asta/ObjectType.java
@@ -1,8 +1,36 @@
+/*
+ * file:       ObjectType.java
+ * author:     Jon Iles
+ * copyright:  (c) Timephased Ltd 2023
+ * date:       2023-02-04
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package net.sf.mpxj.asta;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Identifies the different entities to which user defined fields may be attached
+ * in an Asta schedule.
+ */
 enum ObjectType
 {
    BAR_OBJECT_TYPE(16),
@@ -12,11 +40,22 @@ enum ObjectType
    PERMANENT_RESOURCE_OBJECT_TYPE(51),
    PERMANENT_SCHEDULE_ALLOCATION_OBJECT_TYPE(59);
 
+   /**
+    * Constructor.
+    *
+    * @param value object type id
+    */
    ObjectType(int value)
    {
       m_value = value;
    }
 
+   /**
+    * Retrieve an ObjectType instance by ID.
+    *
+    * @param value ID value
+    * @return ObjectType instance
+    */
    public static ObjectType getInstance(Integer value)
    {
       return MAP.get(value);
