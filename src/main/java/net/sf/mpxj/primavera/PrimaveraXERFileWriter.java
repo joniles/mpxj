9daa8b6a21e1f74852cc5e5556918b87632d6ef1
joniles <jon.iles@bcs.org.uk>
2023-03-09 16:03:42 +0000

    WIP

--- src/main/java/net/sf/mpxj/primavera/PrimaveraXERFileWriter.java
+++ src/main/java/net/sf/mpxj/primavera/PrimaveraXERFileWriter.java
@@ -723,7 +723,7 @@ public class PrimaveraXERFileWriter extends AbstractProjectWriter
       ACTIVITY_COLUMNS.put("auto_compute_act_flag", Boolean.TRUE);
       ACTIVITY_COLUMNS.put("complete_pct_type", TaskField.PERCENT_COMPLETE_TYPE);
       ACTIVITY_COLUMNS.put("task_type", TaskField.ACTIVITY_TYPE);
-      ACTIVITY_COLUMNS.put("duration_type", null);
+      ACTIVITY_COLUMNS.put("duration_type", TaskField.TYPE);
       ACTIVITY_COLUMNS.put("status_code", null);
       ACTIVITY_COLUMNS.put("task_code", null);
       ACTIVITY_COLUMNS.put("task_name", null);
