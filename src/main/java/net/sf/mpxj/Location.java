987ace2ad59102a2233d87ab7a4b4f2adca8a1f2
joniles <jon.iles@bcs.org.uk>
2023-04-18 10:18:38 +0100

    WIP

--- src/main/java/net/sf/mpxj/Location.java
+++ src/main/java/net/sf/mpxj/Location.java
@@ -1,7 +1,38 @@
+/*
+ * file:       Location.java
+ * author:     Jon Iles
+ * copyright:  (c) Packwood Software
+ * date:       2023-04-18
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package net.sf.mpxj;
 
+/**
+ * Represents a location, use to tag projects, resources and activities.
+ */
 public class Location implements ProjectEntityWithUniqueID
 {
+   /**
+    * Constructor used by builder.
+    *
+    * @param builder location builder
+    */
    private Location(Builder builder)
    {
       m_addressLine1 = builder.m_addressLine1;
@@ -15,81 +46,156 @@ public class Location implements ProjectEntityWithUniqueID
       m_municipality = builder.m_municipality;
       m_name = builder.m_name;
       m_uniqueID = builder.m_uniqueID;
-      m_postalCode = builder.m_postalCode;;
+      m_postalCode = builder.m_postalCode;
       m_state = builder.m_state;
       m_stateCode = builder.m_stateCode;
    }
 
+   /**
+    * Retrieve the unique ID.
+    *
+    * @return unique ID
+    */
    @Override public Integer getUniqueID()
    {
       return m_uniqueID;
    }
 
+   /**
+    * Set the unique ID.
+    *
+    * @param id Unique ID value
+    */
    @Override public void setUniqueID(Integer id)
    {
       throw new UnsupportedOperationException();
    }
 
+   /**
+    * Retrieve a line of the address.
+    *
+    * @return address line
+    */
    public String getAddressLine1()
    {
       return m_addressLine1;
    }
 
+   /**
+    * Retrieve a line of the address.
+    *
+    * @return address line
+    */
    public String getAddressLine2()
    {
       return m_addressLine2;
    }
 
+   /**
+    * Retrieve a line of the address.
+    *
+    * @return address line
+    */
    public String getAddressLine3()
    {
       return m_addressLine3;
    }
 
+   /**
+    * Retrieve the city.
+    *
+    * @return city
+    */
    public String getCity()
    {
       return m_city;
    }
 
+   /**
+    * Retrieve the country.
+    *
+    * @return country
+    */
    public String getCountry()
    {
       return m_country;
    }
 
+   /**
+    * Retrieve the country code.
+    *
+    * @return country code
+    */
    public String getCountryCode()
    {
       return m_countryCode;
    }
 
+   /**
+    * Retrieve the latitude.
+    *
+    * @return latitude
+    */
    public Double getLatitude()
    {
       return m_latitude;
    }
 
+   /**
+    * Retrieve the longitude.
+    *
+    * @return longitude
+    */
    public Double getLongitude()
    {
       return m_longitude;
    }
 
+   /**
+    * Retrieve the municipality.
+    *
+    * @return municipality
+    */
    public String getMunicipality()
    {
       return m_municipality;
    }
 
+   /**
+    * Retrieve the name.
+    *
+    * @return name
+    */
    public String getName()
    {
       return m_name;
    }
 
+   /**
+    * Retrieve the postal code.
+    *
+    * @return postal code
+    */
    public String getPostalCode()
    {
       return m_postalCode;
    }
 
+   /**
+    * Retrieve the state.
+    *
+    * @return state
+    */
    public String getState()
    {
       return m_state;
    }
 
+   /**
+    * Retrieve the state code.
+    *
+    * @return state code
+    */
    public String getStateCode()
    {
       return m_stateCode;
@@ -110,92 +216,184 @@ public class Location implements ProjectEntityWithUniqueID
    private final String m_state;
    private final String m_stateCode;
 
+   /**
+    * Location builder.
+    */
    public static class Builder
    {
+      /**
+       * Add an address line.
+       *
+       * @param value address line
+       * @return builder
+       */
       public Builder addressLine1(String value)
       {
          m_addressLine1 = value;
          return this;
       }
 
+      /**
+       * Add an address line.
+       *
+       * @param value address line
+       * @return builder
+       */
       public Builder addressLine2(String value)
       {
          m_addressLine2 = value;
          return this;
       }
 
+      /**
+       * Add an address line.
+       *
+       * @param value address line
+       * @return builder
+       */
       public Builder addressLine3(String value)
       {
          m_addressLine3 = value;
          return this;
       }
 
+      /**
+       * Add a city.
+       *
+       * @param value city
+       * @return builder
+       */
       public Builder city(String value)
       {
          m_city = value;
          return this;
       }
 
+      /**
+       * Add a country.
+       *
+       * @param value country
+       * @return builder
+       */
       public Builder country(String value)
       {
          m_country = value;
          return this;
       }
 
+      /**
+       * Add a country code.
+       *
+       * @param value country code
+       * @return builder
+       */
       public Builder countryCode(String value)
       {
          m_countryCode = value;
          return this;
       }
 
+      /**
+       * Add the latitude.
+       *
+       * @param value latitude
+       * @return builder
+       */
       public Builder latitude(Double value)
       {
          m_latitude = value;
          return this;
       }
 
+      /**
+       * Add the longitude.
+       *
+       * @param value longitude
+       * @return builder
+       */
       public Builder longitude(Double value)
       {
          m_longitude = value;
          return this;
       }
 
+      /**
+       * Add the municipality.
+       *
+       * @param value municipality
+       * @return builder
+       */
       public Builder municipality(String value)
       {
          m_municipality = value;
          return this;
       }
 
+      /**
+       * Add the name.
+       *
+       * @param value name
+       * @return builder
+       */
       public Builder name(String value)
       {
          m_name = value;
          return this;
       }
 
+      /**
+       * Add the unique ID.
+       *
+       * @param value unique ID
+       * @return builder
+       */
       public Builder uniqueID(Integer value)
       {
          m_uniqueID = value;
          return this;
       }
 
+      /**
+       * Add the postal code.
+       *
+       * @param value postal code
+       * @return builder
+       */
       public Builder postalCode(String value)
       {
          m_postalCode = value;
          return this;
       }
 
+      /**
+       * Add the state.
+       *
+       * @param value state
+       * @return builder
+       */
       public Builder state(String value)
       {
          m_state = value;
          return this;
       }
 
+      /**
+       * Add the sate code.
+       *
+       * @param value state code
+       * @return builder
+       */
       public Builder stateCode(String value)
       {
          m_stateCode = value;
          return this;
       }
 
+      /**
+       * Build a Location instance.
+       *
+       * @return Location instance
+       */
       public Location build()
       {
          return new Location(this);
