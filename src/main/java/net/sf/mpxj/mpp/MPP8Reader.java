531c987dafac8078c6d71d5e6b2e92e7c8e3a8e4
joniles <jon.iles@bcs.org.uk>
2023-05-01 17:04:16 +0100

    WIP

--- src/main/java/net/sf/mpxj/mpp/MPP8Reader.java
+++ src/main/java/net/sf/mpxj/mpp/MPP8Reader.java
@@ -478,7 +478,7 @@ final class MPP8Reader implements MPPVariantReader
          flags[2] = (byte) (data[270] & data[305]);
 
          task = m_file.addTask();
-         System.out.println(ByteArrayHelper.hexdump(data, false));
+
          task.setActualCost(NumberHelper.getDouble(((double) MPPUtility.getLong6(data, 234)) / 100));
          task.setActualDuration(MPPUtility.getAdjustedDuration(properties, MPPUtility.getInt(data, 74), MPPUtility.getDurationTimeUnits(MPPUtility.getShort(data, 72), defaultProjectTimeUnits)));
          task.setActualFinish(MPPUtility.getTimestamp(data, 108));
