25a7c58d9037653162f395d757395209eb53fdcb
Jon Iles <jon.iles@bcs.org.uk>
2023-06-19 09:18:13 +0100

    Remove unused imports

--- src/main/java/net/sf/mpxj/mpd/MPD9DatabaseReader.java
+++ src/main/java/net/sf/mpxj/mpd/MPD9DatabaseReader.java
@@ -24,12 +24,10 @@
 package net.sf.mpxj.mpd;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
