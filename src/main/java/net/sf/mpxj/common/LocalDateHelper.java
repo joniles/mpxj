0ad13d845b18f4728bf80bf550e9828ae14269c9
Jon Iles <jon.iles@bcs.org.uk>
2023-06-26 11:12:54 +0100

    WIP

--- src/main/java/net/sf/mpxj/common/LocalDateHelper.java
+++ src/main/java/net/sf/mpxj/common/LocalDateHelper.java
@@ -1,10 +1,43 @@
+/*
+ * file:       LocalDateHelper.java
+ * author:     Jon Iles
+ * date:       2023-06-26
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package net.sf.mpxj.common;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+/**
+ * Common code for working with LocalDate instances.
+ */
 public final class LocalDateHelper
 {
+   /**
+    * Convert a LocalDate instance to a LOcalDateTime instance,
+    * handling null values.
+    * 
+    * @param date LocalDate instance
+    * @return LocalDateTime instance or null
+    */
    public static LocalDateTime getLocalDateTime(LocalDate date)
    {
       if (date == null)
