165bf946fbe2af2f1cefb42eef06c17ff20e7799
joniles <jon.iles@bcs.org.uk>
2023-08-19 20:25:41 +0100

    Javadoc

--- src/main/java/net/sf/mpxj/TotalSlackCalculationType.java
+++ src/main/java/net/sf/mpxj/TotalSlackCalculationType.java
@@ -1,4 +1,29 @@
+/*
+ * file:       TotalSlackCalculationType.java
+ * author:     Jon Iles
+ * date:       2023-08-19
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
 package net.sf.mpxj;
+
+/**
+ * Enum representing the different calculation types which can be uised to determine total slack.
+ */
 public enum TotalSlackCalculationType
 {
    SMALLEST_SLACK,
