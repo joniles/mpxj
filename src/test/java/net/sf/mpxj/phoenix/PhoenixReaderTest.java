585539df6bbdd8132c69e26ce393f642f08ac4e4
Rohit Sinha <rsinha@smartpmtech.com>
2023-09-13 10:27:39 -0400

    Issue-565 - Dropping ResourceLoader for MpxjTestData

--- src/test/java/net/sf/mpxj/phoenix/PhoenixReaderTest.java
+++ src/test/java/net/sf/mpxj/phoenix/PhoenixReaderTest.java
@@ -1,11 +1,8 @@
 package net.sf.mpxj.phoenix;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import net.sf.mpxj.MPXJException;
 import net.sf.mpxj.ProjectFile;
-import net.sf.mpxj.ResourceLoader;
 import net.sf.mpxj.Task;
+import net.sf.mpxj.junit.MpxjTestData;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
@@ -13,10 +10,10 @@ import static org.junit.Assert.assertNotNull;
 public class PhoenixReaderTest
 {
    @Test
-   public void testPPXFinishDates() throws IOException, URISyntaxException, MPXJException
+   public void testPPXFinishDates() throws MPXJException
    {
       PhoenixReader reader = new PhoenixReader();
-      ProjectFile projectFile = reader.read(ResourceLoader.loadResource("23.07.01 - 00 - PHYSICAL PERCENT COMPLETE.ppx"));
+      ProjectFile projectFile = reader.read(MpxjTestData.filePath("23.07.01 - 00 - PHYSICAL PERCENT COMPLETE.ppx"));
       for (Task task : projectFile.getTasks()) {
          assertNotNull(task.getFinish());
       }
