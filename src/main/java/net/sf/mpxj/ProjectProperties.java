8d6413e00b47f263778ac213fa24fee90d994c1c
Jon Iles <jon.iles@bcs.org.uk>
2023-09-20 17:46:13 +0100

    Ensure getCustomProperties returns an empry map not null (#571)

--- src/main/java/net/sf/mpxj/ProjectProperties.java
+++ src/main/java/net/sf/mpxj/ProjectProperties.java
@@ -2957,12 +2957,12 @@ public final class ProjectProperties extends AbstractFieldContainer<ProjectPrope
       return getParentFile().getLatestFinishDate();
    }
 
-   private Integer calculateDaysPerMonth()
+   private Integer defaultDaysPerMonth()
    {
       return DEFAULT_DAYS_PER_MONTH;
    }
 
-   private Integer calculateMinutesPerDay()
+   private Integer defaultMinutesPerDay()
    {
       return DEFAULT_MINUTES_PER_DAY;
    }
@@ -2982,29 +2982,34 @@ public final class ProjectProperties extends AbstractFieldContainer<ProjectPrope
       return Integer.valueOf(NumberHelper.getInt(getMinutesPerDay()) * NumberHelper.getInt(getDaysPerMonth()) * 12);
    }
 
-   private Character calculateDateSeparator()
+   private Character defaultDateSeparator()
    {
       return Character.valueOf(DEFAULT_DATE_SEPARATOR);
    }
 
-   private Character calculateTimeSeparator()
+   private Character defaultTimeSeparator()
    {
       return Character.valueOf(DEFAULT_TIME_SEPARATOR);
    }
 
-   private Character calculateDecimalSeparator()
+   private Character defaultDecimalSeparator()
    {
       return Character.valueOf(DEFAULT_DECIMAL_SEPARATOR);
    }
 
-   private Character calculateThousandsSeparator()
+   private Character defaultThousandsSeparator()
    {
       return Character.valueOf(DEFAULT_THOUSANDS_SEPARATOR);
    }
 
-   private Character calculateMpxDelimiter()
+   private Character defaultMpxDelimiter()
    {
       return Character.valueOf(DEFAULT_MPX_DELIMITER);
+   }
+
+   private Map<String, Object> defaultCustomProperties()
+   {
+      return new HashMap<String, Object>();
    }
 
    /**
@@ -3122,15 +3127,16 @@ public final class ProjectProperties extends AbstractFieldContainer<ProjectPrope
    {
       CALCULATED_FIELD_MAP.put(ProjectField.START_DATE, ProjectProperties::calculateStartDate);
       CALCULATED_FIELD_MAP.put(ProjectField.FINISH_DATE, ProjectProperties::calculateFinishDate);
-      CALCULATED_FIELD_MAP.put(ProjectField.DAYS_PER_MONTH, ProjectProperties::calculateDaysPerMonth);
-      CALCULATED_FIELD_MAP.put(ProjectField.MINUTES_PER_DAY, ProjectProperties::calculateMinutesPerDay);
       CALCULATED_FIELD_MAP.put(ProjectField.MINUTES_PER_WEEK, ProjectProperties::calculateMinutesPerWeek);
       CALCULATED_FIELD_MAP.put(ProjectField.MINUTES_PER_MONTH, ProjectProperties::calculateMinutesPerMonth);
       CALCULATED_FIELD_MAP.put(ProjectField.MINUTES_PER_YEAR, ProjectProperties::calculateMinutesPerYear);
-      CALCULATED_FIELD_MAP.put(ProjectField.DATE_SEPARATOR, ProjectProperties::calculateDateSeparator);
-      CALCULATED_FIELD_MAP.put(ProjectField.TIME_SEPARATOR, ProjectProperties::calculateTimeSeparator);
-      CALCULATED_FIELD_MAP.put(ProjectField.THOUSANDS_SEPARATOR, ProjectProperties::calculateThousandsSeparator);
-      CALCULATED_FIELD_MAP.put(ProjectField.DECIMAL_SEPARATOR, ProjectProperties::calculateDecimalSeparator);
-      CALCULATED_FIELD_MAP.put(ProjectField.MPX_DELIMITER, ProjectProperties::calculateMpxDelimiter);
+      CALCULATED_FIELD_MAP.put(ProjectField.DAYS_PER_MONTH, ProjectProperties::defaultDaysPerMonth);
+      CALCULATED_FIELD_MAP.put(ProjectField.MINUTES_PER_DAY, ProjectProperties::defaultMinutesPerDay);
+      CALCULATED_FIELD_MAP.put(ProjectField.DATE_SEPARATOR, ProjectProperties::defaultDateSeparator);
+      CALCULATED_FIELD_MAP.put(ProjectField.TIME_SEPARATOR, ProjectProperties::defaultTimeSeparator);
+      CALCULATED_FIELD_MAP.put(ProjectField.THOUSANDS_SEPARATOR, ProjectProperties::defaultThousandsSeparator);
+      CALCULATED_FIELD_MAP.put(ProjectField.DECIMAL_SEPARATOR, ProjectProperties::defaultDecimalSeparator);
+      CALCULATED_FIELD_MAP.put(ProjectField.MPX_DELIMITER, ProjectProperties::defaultMpxDelimiter);
+      CALCULATED_FIELD_MAP.put(ProjectField.CUSTOM_PROPERTIES, ProjectProperties::defaultCustomProperties);
    }
 }
