ad4eea772981da31c7d966c213e10ce191fb64ba
joniles <jon.iles@bcs.org.uk>
2023-11-20 17:44:25 +0000

    WIP

--- src/main/java/net/sf/mpxj/RelationContainer.java
+++ src/main/java/net/sf/mpxj/RelationContainer.java
@@ -25,7 +25,6 @@ package net.sf.mpxj;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
