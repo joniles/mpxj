1b7f4b75c304dea3e310cea1bdbd57b5a28406f4
joniles <jon.iles@bcs.org.uk>
2023-11-20 17:45:21 +0000

    WIP

--- src/main/java/net/sf/mpxj/Task.java
+++ src/main/java/net/sf/mpxj/Task.java
@@ -27,11 +27,9 @@ package net.sf.mpxj;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
