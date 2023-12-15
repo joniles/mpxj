66de1043a491b1d989b42fa5eb1c4b927c561241
joniles <jon.iles@bcs.org.uk>
2023-12-15 18:31:40 +0000

    WIP

--- src/main/java/net/sf/mpxj/primavera/PrimaveraXERFileWriter.java
+++ src/main/java/net/sf/mpxj/primavera/PrimaveraXERFileWriter.java
@@ -36,7 +36,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
