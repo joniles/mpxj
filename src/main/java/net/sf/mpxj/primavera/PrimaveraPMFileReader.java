c7778b48ba3f467af24d5c33aa9cc0f7cb8e436d
joniles <jon.iles@bcs.org.uk>
2024-03-02 13:04:42 +0000

    WIP

--- src/main/java/net/sf/mpxj/primavera/PrimaveraPMFileReader.java
+++ src/main/java/net/sf/mpxj/primavera/PrimaveraPMFileReader.java
@@ -30,7 +30,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 
 import java.util.HashMap;
