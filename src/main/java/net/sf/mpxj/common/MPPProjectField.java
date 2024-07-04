3458c4347f9d48d2ea7d794f20cf1a87774e57f3
Jon Iles <jon.iles@bcs.org.uk>
2024-07-04 15:45:48 +0100

    Mpp udf unique (#713)

--- src/main/java/net/sf/mpxj/common/MPPProjectField.java
+++ src/main/java/net/sf/mpxj/common/MPPProjectField.java
@@ -52,7 +52,7 @@ public class MPPProjectField
             int id = (k.intValue() & 0xFFF) + 1;
             String internalName = "ENTERPRISE_CUSTOM_FIELD" + id;
             String externalName = "Enterprise Custom Field " + id;
-            return new UserDefinedField(k, internalName, externalName, FieldTypeClass.PROJECT, false, DataType.CUSTOM);
+            return new UserDefinedField(Integer.valueOf(PROJECT_FIELD_BASE + k.intValue()), internalName, externalName, FieldTypeClass.PROJECT, false, DataType.CUSTOM);
          });
       }
 
