10831688b7e816c5c53fdf3ee2df0afeea77d8f3
joniles <jon@timephased.com>
2024-12-11 11:51:36 +0000

    WIP

--- src/main/java/net/sf/mpxj/Code.java
+++ src/main/java/net/sf/mpxj/Code.java
@@ -1,7 +1,33 @@
+/*
+ * file:       Code.java
+ * author:     Jon Iles
+ * date:       2024-12-10
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
 package net.sf.mpxj;
 
 import java.util.List;
 
+/**
+ * Interface implemented by classes representing Primavera P6 codes.
+ */
 public interface Code extends ProjectEntityWithUniqueID
 {
    /**
@@ -39,7 +65,17 @@ public interface Code extends ProjectEntityWithUniqueID
     */
    Integer getMaxLength();
 
+   /**
+    * Retrieve all values for this code.
+    *
+    * @return list of all values
+    */
    List<? extends CodeValue> getValues();
 
+   /**
+    * Retrieve the immediate child values for this code.
+    *
+    * @return list of child values
+    */
    List<? extends CodeValue> getChildValues();
 }
