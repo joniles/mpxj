ae1d91a0195169b7abbcdd5980754db22ab9f92d
joniles <jon@timephased.com>
2025-01-22 10:25:30 +0000

    WIP

--- src/main/java/net/sf/mpxj/cpm/CpmTest.java
+++ src/main/java/net/sf/mpxj/cpm/CpmTest.java
@@ -250,6 +250,7 @@ public class CpmTest
       EXCLUDED_FILES.add("invalid-calendar-data.encoding.xer");
       EXCLUDED_FILES.add("duplicate-relation-uid.xer");
       EXCLUDED_FILES.add("hotter-gunmen.xml");
+      EXCLUDED_FILES.add("kindly-dissolve.xml");
 
       // Aligns with MPXJ when scheduled
       EXCLUDED_FILES.add("teenage-contest.encoding.xer");
@@ -309,9 +310,9 @@ public class CpmTest
       // early/late start/finish. There appears to be a difference...
       EXCLUDED_FILES.add("fleet-salary.xml");
 
-      // Interesting insight - Late Finish w/o successors governed by parent WBS late finish.
-      // Need to investigate - may resolve ALAP weirdness???
+      // Calculation of late finish date for activity with no successors
       EXCLUDED_FILES.add("prime-chiropractor-scheduled.xml");
+      EXCLUDED_FILES.add("golden-aperture.xml");
 
       // Forward pass logic differs from other working files - can't see why
       EXCLUDED_FILES.add("naval-cancer.xml");
