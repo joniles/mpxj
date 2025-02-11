0eaa6e200a75fcc6309cd55a0bd22443f927257c
joniles <jon@timephased.com>
2025-02-11 13:44:16 +0000

    WIP

--- src/main/java/net/sf/mpxj/cpm/PrimaveraScheduler.java
+++ src/main/java/net/sf/mpxj/cpm/PrimaveraScheduler.java
@@ -10,7 +10,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import net.sf.mpxj.ActivityStatus;
 import net.sf.mpxj.ActivityType;
 import net.sf.mpxj.ConstraintType;
 import net.sf.mpxj.DayType;
