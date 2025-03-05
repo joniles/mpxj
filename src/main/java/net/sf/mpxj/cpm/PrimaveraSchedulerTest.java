17b1ed3182ab345b9991b6a78567c232f08bde23
joniles <jon.iles@bcs.org.uk>
2025-03-05 11:28:13 +0000

    WIP

--- src/main/java/net/sf/mpxj/cpm/PrimaveraSchedulerTest.java
+++ src/main/java/net/sf/mpxj/cpm/PrimaveraSchedulerTest.java
@@ -101,7 +101,13 @@ public class PrimaveraSchedulerTest
 
       try
       {
-         scheduler.process(m_workingFile.getProjectProperties().getPlannedStart());
+         LocalDateTime start = m_workingFile.getProjectProperties().getPlannedStart();
+         if (start == null)
+         {
+            start = m_workingFile.getProjectProperties().getStartDate();
+         }
+
+         scheduler.process(start);
       }
 
       catch(CpmException ex)
@@ -340,6 +346,7 @@ public class PrimaveraSchedulerTest
       USE_SCHEDULED_COPY.add("doubtful-contractor.xer");
       USE_SCHEDULED_COPY.add("aloof-proton.xer");
       USE_SCHEDULED_COPY.add("outstanding-vaudeville.xer");
+      USE_SCHEDULED_COPY.add("warm-bastion.encoding.xer");
    }
 
    private static final Set<String> NO_WBS_TEST = new HashSet<>();
