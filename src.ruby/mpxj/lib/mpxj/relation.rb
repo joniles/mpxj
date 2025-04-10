cfd3196129f906a2967c0283453bb19c6a8b1fec
joniles <jon@timephased.com>
2025-04-10 15:29:58 +0100

    WIP

--- src.ruby/mpxj/lib/mpxj/relation.rb
+++ src.ruby/mpxj/lib/mpxj/relation.rb
@@ -1,14 +1,6 @@
 module MPXJ
   # Represents a relationship between two tasks in a project plan
   class Relation < Container
-    # Retrieve the Task Unique ID value
-    # <b>DEPRECATED:</b> Please use <tt>predecessor_task_unique_id</tt> or <tt>successor_task_unique_id</tt>instead.
-    # 
-    # @return Task Unique ID value
- 		def task_unique_id
- 			get_integer_value(attribute_values['task_unique_id'])
-  	end
-
     # Retrieve the Predecessor Task Unique ID value
     #
     # @return Predecessor Task Unique ID value
