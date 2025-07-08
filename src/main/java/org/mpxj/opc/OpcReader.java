33cad2f4377a66d795c9b7f7e219f85000e6cb6d
joniles <jon@timephased.com>
2025-07-08 13:04:58 +0100

    WIP

--- src/main/java/org/mpxj/opc/OpcReader.java
+++ src/main/java/org/mpxj/opc/OpcReader.java
@@ -50,12 +50,12 @@ public class OpcReader
       reader.setLogger(log, Level.INFO);
 
 
-//      List<OpcProject> projects = reader.getProjects();
-//      projects.forEach(System.out::println);
+      List<OpcProject> projects = reader.getProjects();
+      projects.forEach(System.out::println);
 
-      OpcProject project = new OpcProject();
-      project.setProjectId(14501);
-      project.setWorkspaceId(6003);
+//      OpcProject project = new OpcProject();
+//      project.setProjectId(14501);
+//      project.setWorkspaceId(6003);
 
         //List<OpcProjectBaseline> baselines = reader.getProjectBaselines(project);
 
