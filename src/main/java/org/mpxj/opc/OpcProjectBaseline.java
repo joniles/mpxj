d6726c9061bb78d280275b1ff0635f9d103b18e0
joniles <jon@timephased.com>
2025-07-09 20:49:55 +0100

    WIP

--- src/main/java/org/mpxj/opc/OpcProjectBaseline.java
+++ src/main/java/org/mpxj/opc/OpcProjectBaseline.java
@@ -1,22 +1,67 @@
+/*
+ * file:       OpcProjectBaseline.java
+ * author:     Jon Iles
+ * date:       2025-07-09
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
 package org.mpxj.opc;
 
+/**
+ * Represents a baseline in OPC when baselines for a project are listed,
+ * and used to request export of a baseline as part of a project export.
+ */
 public class OpcProjectBaseline
 {
+   /**
+    * Retrieve the baseline ID.
+    *
+    * @return baseline ID
+    */
    public long getProjectBaselineId()
    {
       return m_projectBaselineId;
    }
 
+   /**
+    * Sets the baseline ID.
+    *
+    * @param projectBaselineId baseline ID
+    */
    public void setProjectBaselineId(long projectBaselineId)
    {
       m_projectBaselineId = projectBaselineId;
    }
 
+   /**
+    * Retrieve the baseline name.
+    *
+    * @return baseline name
+    */
    public String getName()
    {
       return m_name;
    }
 
+   /**
+    * Sets the baseline name.
+    *
+    * @param name baseline name
+    */
    public void setName(String name)
    {
       m_name = name;
--- src/main/java/org/mpxj/opc/Workspace.java
+++ src/main/java/org/mpxj/opc/Workspace.java
@@ -1,22 +1,66 @@
+/*
+ * file:       Workspace.java
+ * author:     Jon Iles
+ * date:       2025-07-09
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
 package org.mpxj.opc;
 
+/**
+ * Represents the JSON payload for an OPC workspace.
+ */
 class Workspace
 {
+   /**
+    * Retrieve the workspace ID.
+    *
+    * @return workspace ID
+    */
    public long getWorkspaceId()
    {
       return m_workspaceId;
    }
 
+   /**
+    * Sets the workspace ID.
+    *
+    * @param workspaceId workspace ID
+    */
    public void setWorkspaceId(long workspaceId)
    {
       m_workspaceId = workspaceId;
    }
 
+   /**
+    * Retrieves the workspace name.
+    *
+    * @return workspace name
+    */
    public String getWorkspaceName()
    {
       return m_workspaceName;
    }
 
+   /**
+    * Sets the workspace name.
+    *
+    * @param workspaceName workspace name
+    */
    public void setWorkspaceName(String workspaceName)
    {
       m_workspaceName = workspaceName;
