91f0f61238f350c61c91258b94b5e46274479a50
joniles <jon@timephased.com>
2025-11-24 09:40:54 +0000

    WIP

--- src/test/java/org/mpxj/junit/MpxjTestRunner.java
+++ src/test/java/org/mpxj/junit/MpxjTestRunner.java
@@ -4,30 +4,36 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
+import org.junit.jupiter.engine.JupiterTestEngine;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherConfig;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.LoggingListener;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 public class MpxjTestRunner
 {
-   public static void main(String[] argv)
-   {
-      run();
-   }
+//   public static void main(String[] argv)
+//   {
+//      List<String> classNames = Arrays.asList("org.mpxj.junit.AvailabilityTest", "org.mpxj.junit.DateUtilityTest");
+//      run(classNames);
+//   }
 
-   public static void run()
+   public static void run(List<String> classNames)
    {
       Logger logger = Logger.getLogger(LoggingListener.class.getName());
       logger.setLevel(Level.ALL);
@@ -45,14 +51,43 @@ public class MpxjTestRunner
       handler.setLevel(Level.ALL);
       logger.addHandler(handler);
 
-      TestExecutionListener listener = LoggingListener.forJavaUtilLogging();
-      LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
-         .selectors(selectPackage("org.mpxj.junit"))
+      LauncherConfig config = LauncherConfig.builder()
+         .enableTestEngineAutoRegistration(false)
+         .addTestEngines(new JupiterTestEngine())
          .build();
-      Launcher launcher = LauncherFactory.create();
+
+      LauncherDiscoveryRequestBuilder builder = LauncherDiscoveryRequestBuilder.request();
+      for (String className : classNames)
+      {
+         try
+         {
+            builder.selectors(selectClass(Class.forName(className)));
+         }
+
+         catch (ClassNotFoundException ex)
+         {
+            System.out.println("Unable to load " + className);
+         }
+      }
+      LauncherDiscoveryRequest request =  builder.build();
+
+      SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+      Launcher launcher = LauncherFactory.create(config);
       TestPlan testPlan = launcher.discover(request);
-      launcher.registerTestExecutionListeners(listener);
+      launcher.registerTestExecutionListeners(LoggingListener.forJavaUtilLogging(), listener);
       launcher.execute(request);
+
+      TestExecutionSummary summary = listener.getSummary();
+      System.out.println();
+      if (summary.getTotalFailureCount() == 0)
+      {
+         System.out.println("SUCCESS");
+      }
+      else
+      {
+         System.out.println("FAILED");
+      }
    }
 
    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss");
