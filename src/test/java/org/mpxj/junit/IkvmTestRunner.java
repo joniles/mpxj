9fe508cdc1303a7d390b225766ddbdd6920139fa
joniles <jon@timephased.com>
2025-11-24 10:56:55 +0000

    WIP

--- src/test/java/org/mpxj/junit/IkvmTestRunner.java
+++ src/test/java/org/mpxj/junit/IkvmTestRunner.java
@@ -1,10 +1,31 @@
+/*
+ * file:       IkvmTestRunner.java
+ * author:     Jon Iles
+ * date:       2025-11-24
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package org.mpxj.junit;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
@@ -25,14 +46,27 @@ import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
-public class MpxjTestRunner
+/**
+ * This class is provided to make it easier to invoke JUnit 5 tests from .Net code.
+ * The problem I've encountered is that the various translated assemblies have their
+ * own classloaders, and without explicit references in code, can't dynamically
+ * retrieve classes available in other assemblies, therefore JUnit 5 can't
+ * discover the available test classes.
+ * <p/>
+ * The second issue is that I haven't been able to get translated Java code to
+ * enumerate the available classes.
+ * <p/>
+ * The result is that a .Net "driver" class is used to list the test classes from the
+ * `org.mpxj.junit` assembly, then passes the list of classes to the `run` method
+ * below.
+ */
+public class IkvmTestRunner
 {
-//   public static void main(String[] argv)
-//   {
-//      List<String> classNames = Arrays.asList("org.mpxj.junit.AvailabilityTest", "org.mpxj.junit.DateUtilityTest");
-//      run(classNames);
-//   }
-
+   /**
+    * Given a list of fully qualified class names, run the tests they represent.
+    *
+    * @param classNames list of class names
+    */
    public static void run(List<String> classNames)
    {
       Logger logger = Logger.getLogger(LoggingListener.class.getName());
--- src/test/java/org/mpxj/junit/MpxjTestData.java
+++ src/test/java/org/mpxj/junit/MpxjTestData.java
@@ -93,5 +93,4 @@ public class MpxjTestData
       }
       return result;
    }
-
 }
