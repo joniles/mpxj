6db42bc31f99fc655d012907cf0737f49ff8a68a
Jon Iles <jon@timephased.com>
2026-01-22 13:17:56 +0000

    Update PMXML schema to 25.12 (#884)

--- src/main/java/org/mpxj/primavera/NamespaceFilter.java
+++ src/main/java/org/mpxj/primavera/NamespaceFilter.java
@@ -74,5 +74,5 @@ class NamespaceFilter extends XMLFilterImpl
       ELEMENT_MAP.put("BusinessObjects", "APIBusinessObjects");
    }
 
-   private static final String NAMESPACE = "http://xmlns.oracle.com/Primavera/P6/V24.12/API/BusinessObjects";
+   private static final String NAMESPACE = "http://xmlns.oracle.com/Primavera/P6/V25.12/API/BusinessObjects";
 }
