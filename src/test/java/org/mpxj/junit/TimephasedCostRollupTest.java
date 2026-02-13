078d0d86f0fe2113817aeb55da22d14230df2282
joniles <jon@timephased.com>
2026-02-13 15:40:16 +0000

    WIP

--- src/test/java/org/mpxj/junit/TimephasedCostRollupTest.java
+++ src/test/java/org/mpxj/junit/TimephasedCostRollupTest.java
@@ -1,3 +1,25 @@
+/*
+ * file:       TimephasedCostRollupTest.java
+ * author:     Jon Iles
+ * date:       2026-02-13
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package org.mpxj.junit;
 
 import java.time.LocalDateTime;
@@ -13,9 +35,15 @@ import org.mpxj.utility.TimescaleUtility;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Test task cost rollup.
+ */
 @SuppressWarnings("boxing") public class TimephasedCostRollupTest
 {
-   @Test public void testCostRollup() throws Exception
+   /**
+    * Test task cost rollup.
+    */
+   @Test public void testTaskCostRollup() throws Exception
    {
       List<LocalDateTimeRange> rangeCoversAssignment = new TimescaleUtility().createTimescale(LocalDateTime.of(2026, 2, 11, 0, 0), TimescaleUnits.DAYS, 10);
 
