49944d318732ab6bcb95d54fdc5ae6d3e91ff258
joniles <jon@timephased.com>
2026-02-13 14:57:55 +0000

    WIP

--- src/test/java/org/mpxj/junit/LocalDateTimeHelperTest.java
+++ src/test/java/org/mpxj/junit/LocalDateTimeHelperTest.java
@@ -1,3 +1,25 @@
+/*
+ * file:       LocalDateTimeHelperTest.java
+ * author:     Jon Iles
+ * date:       2026-02-13
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package org.mpxj.junit;
 
 import java.time.LocalDateTime;
@@ -7,8 +29,14 @@ import org.mpxj.common.LocalDateTimeHelper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * LocalDateTimeHelper help method tests.
+ */
 public class LocalDateTimeHelperTest
 {
+   /**
+    * Test  range comparison method.
+    */
    @Test public void rangeComparisonTests()
    {
       LocalDateTime startRange1 = LocalDateTime.of(2026, 2, 1, 9, 0);
