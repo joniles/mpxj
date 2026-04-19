0ad5abbc4ab53f383ccbc502e34ccb118f3f7930
joniles <jon.iles@bcs.org.uk>
2026-04-19 17:20:43 +0100

    WIP

--- src/main/java/org/mpxj/mpp/DataAtOffset.java
+++ src/main/java/org/mpxj/mpp/DataAtOffset.java
@@ -1,3 +1,25 @@
+/*
+ * file:       DataAtOffset.java
+ * author:     Jon Iles
+ * date:       2026-04-19
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package org.mpxj.mpp;
 
 import java.time.LocalDate;
@@ -10,8 +32,19 @@ import org.mpxj.TimeUnit;
 import org.mpxj.common.ByteArrayHelper;
 import org.mpxj.common.LocalDateHelper;
 
+/**
+ * Class used to assist understanding data storage in an MPP file.
+ * Given a byte array and an offset, generate the different values
+ * which that data might represent.
+ */
 public class DataAtOffset
 {
+   /**
+    * Constructor.
+    *
+    * @param data bye array
+    * @param offset offset into byte array
+    */
    public DataAtOffset(byte[] data, int offset)
    {
       //
@@ -92,61 +125,121 @@ public class DataAtOffset
       }
    }
 
+   /**
+    * Retrieve a short value.
+    *
+    * @return short value
+    */
    public Integer getShort()
    {
       return m_short;
    }
 
+   /**
+    * Retrieve a time unit value.
+    *
+    * @return time unit value
+    */
    public TimeUnit getTimeUnit()
    {
       return m_timeUnit;
    }
 
+   /**
+    * Retrieve a percentage value.
+    *
+    * @return percentage value
+    */
    public Double getPercentage()
    {
       return m_percentage;
    }
 
+   /**
+    * Retrieve a date value.
+    *
+    * @return date value
+    */
    public LocalDate getDate()
    {
       return m_date;
    }
 
+   /**
+    * Retrieve a time value.
+    *
+    * @return time value
+    */
    public LocalTime getTime()
    {
       return m_time;
    }
 
+   /**
+    * Retrieve a work time unit value.
+    *
+    * @return work time unit value
+    */
    public TimeUnit getWorkTimeUnit()
    {
       return m_workTimeUnit;
    }
 
+   /**
+    * Retrieve a timestamp value.
+    *
+    * @return timestamp value
+    */
    public LocalDateTime getTimestamp()
    {
       return m_timestamp;
    }
 
+   /**
+    * Retrieve a six byte long value.
+    *
+    * @return six byte long value
+    */
    public Long getLongSix()
    {
       return m_longSix;
    }
 
+   /**
+    * Retrieve a long value.
+    *
+    * @return long value
+    */
    public Long getLong()
    {
       return m_long;
    }
 
+   /**
+    * Retrieve a double value.
+    *
+    * @return double value
+    */
    public Double getDouble()
    {
       return m_double;
    }
 
+   /**
+    * Retrieve a duration value.
+    *
+    * @return duration value
+    */
    public Duration getDuration()
    {
       return m_duration;
    }
 
+   /**
+    * Retrieve a UUID value.
+    *
+    * @return UUID value
+    */
    public UUID getGuid()
    {
       return m_guid;
