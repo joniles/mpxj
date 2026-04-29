3719f8c92a3100d188083681ac7ca12c1cb99655
joniles <jon.iles@bcs.org.uk>
2026-04-29 15:28:20 +0100

    WIP

--- src/main/java/org/mpxj/common/ProjectPropertiesHelper.java
+++ src/main/java/org/mpxj/common/ProjectPropertiesHelper.java
@@ -1,5 +1,6 @@
 package org.mpxj.common;
 
+import org.mpxj.ProjectFile;
 import org.mpxj.ProjectProperties;
 
 public final class ProjectPropertiesHelper
@@ -9,11 +10,16 @@ public final class ProjectPropertiesHelper
       String title = properties.getProjectTitle();
       if (title == null || title.isEmpty())
       {
-         //         if (!m_projectFile.getTasks().isEmpty())
-         //         {
-         //            title = m_projectFile.getTasks().get(0).getName();
-         //         }
+         // We don't have an explicit title set.
+         // If the parent project has a single child task
+         // assume that this is a summary task and use
+         // the task name as the project title.
+         if (properties.getParentFile().getChildTasks().size() == 1)
+         {
+            title = properties.getParentFile().getChildTasks().get(0).getName();
+         }
 
+         // If we still don't have a title set, use a default value.
          if (title == null || title.isEmpty())
          {
             title = "Project1";
