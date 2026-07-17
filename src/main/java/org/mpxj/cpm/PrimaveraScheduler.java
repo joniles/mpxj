7eb2d458b7cb3ad8aadefa586bbb429c44d27d35
joniles <jon@timephased.com>
2026-07-17 17:39:47 +0100

    WIP

--- src/main/java/org/mpxj/cpm/PrimaveraScheduler.java
+++ src/main/java/org/mpxj/cpm/PrimaveraScheduler.java
@@ -3979,7 +3979,7 @@ public class PrimaveraScheduler implements Scheduler
       if (task.getActualStart() == null)
       {
          task.setPlannedStart(task.getStart());
-         task.setPlannedFinish(task.getFinish());
+         task.setPlannedFinish(task.getEffectiveCalendar().getDate(task.getPlannedStart(), task.getPlannedDuration()));
       }
 
       if (task.getActualStart() == null || task.getActualFinish() == null)
