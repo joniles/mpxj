37ef3aa29181e2efd55d57e7611bf9db7ad2f1ae
joniles <jon@timephased.com>
2026-07-29 13:13:16 +0100

    WIP

--- src/main/java/org/mpxj/cpm/PrimaveraDateCalculator.java
+++ src/main/java/org/mpxj/cpm/PrimaveraDateCalculator.java
@@ -1,3 +1,25 @@
+/*
+ * file:       PrimaveraDateCalculator.java
+ * author:     Jon Iles
+ * date:       2026-07-29
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package org.mpxj.cpm;
 
 import java.time.DayOfWeek;
@@ -13,6 +35,9 @@ import org.mpxj.ProjectCalendarHours;
 import org.mpxj.ProjectFile;
 import org.mpxj.Relation;
 
+/**
+ * Implementation of common date calculations which are used by the Primavera scheduer and elsewhere.
+ */
 class PrimaveraDateCalculator
 {
    public PrimaveraDateCalculator(ProjectFile file, Supplier<LocalDateTime> dataDateSupplier)
